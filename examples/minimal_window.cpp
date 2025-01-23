46278b69abfb29fc448d8f6be76c1d4a0960a4c6
Miro Palmu <email@miropalmu.cc>
2025-01-23 11:09:10 +0200

    examples: add minimal_window

---
+++ examples/minimal_window.cpp
@@ -0,0 +1,35 @@
+// Copyright (C) 2025 Miro Palmu.
+//
+// This file is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This file is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+#include <utility>
+
+#include "guilander/static_picture.hpp"
+#include "guilander/static_picture_window.hpp"
+
+int
+main() {
+    auto picture = guilander::static_picture(50, 50);
+
+    for (const auto [coords, pixel] : picture.view_pixels()) {
+        pixel =
+            guilander::static_picture::pixel{ { static_cast<std::uint8_t>(10 * coords.x % 255) },
+                                              { static_cast<std::uint8_t>(20 * coords.y % 255) },
+                                              {},
+                                              { 255 } };
+    }
+
+    auto window = guilander::static_picture_window{ std::move(picture) };
+    window.wait_for_close_event();
+}
